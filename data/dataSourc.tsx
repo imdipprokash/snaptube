53498a18b62a3c4b698561fdf3e4442dcc386e5f
Dipprokash <dipprokash2015@gmail.com>
2022-10-06 19:16:17 +0530

    firebase added

---
+++ data/dataSourc.tsx
@@ -0,0 +1,48 @@
+export interface dataInterFace {
+  id: number;
+  title: string;
+  des: string;
+}
+
+export const dataSource: Object[] = [
+  {
+    id: 1,
+    title: 'hlew',
+    des: 'sdfsf',
+  },
+  {
+    id: 2,
+    title: 'hlew',
+    des: 'sdfsf',
+  },
+  {
+    id: 3,
+    title: 'hlew',
+    des: 'sdfsf',
+  },
+  {
+    id: 4,
+    title: 'hlew',
+    des: 'sdfsf',
+  },
+  {
+    id: 5,
+    title: 'hlew',
+    des: 'sdfsf',
+  },
+  {
+    id: 6,
+    title: 'hlew',
+    des: 'sdfsf',
+  },
+  {
+    id: 7,
+    title: 'hlew',
+    des: 'sdfsf',
+  },
+  {
+    id: 8,
+    title: 'hlew',
+    des: 'sdfsf',
+  },
+];
